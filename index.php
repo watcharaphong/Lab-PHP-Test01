f1cf7d3c7a0ccf42b851211f7b7973a051d2ffe9
watcharaphong <87680218+watcharaphong@users.noreply.github.com>
2021-09-15 18:25:17 +0700

    update

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<?php echo '<p>Hello World--My name is ชื่อ-สกุล-รหัสนิสิต</p>'; ?>
+</body>
 </html>
